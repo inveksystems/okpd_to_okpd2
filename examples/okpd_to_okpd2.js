b80e2257de63b1ee73ec2e74b0bad1d0a21537d7
inveksystems <inveksystems@gmail.com>
2017-06-29 16:35:08 +0400

    Update okpd_to_okpd2.js

--- examples/okpd_to_okpd2.js
+++ examples/okpd_to_okpd2.js
@@ -2,8 +2,8 @@ const okpdTranslater = require('../src/okpd_to_okpd2.js')
 
 console.log(okpdTranslater.get('65.77.30')) // 26.30.40.110
 console.log(okpdTranslater.get('98.96.94')) // 01.46.10.210
-console.log(okpdTranslater.get('98.96.99')) // undefined
+console.log(okpdTranslater.get('99.99.99')) // undefined
 
 console.log(okpdTranslater.has('65.77.30')) // true
 console.log(okpdTranslater.has('98.96.94')) // true
-console.log(okpdTranslater.has('98.96.99')) // false
+console.log(okpdTranslater.has('99.99.99')) // false
